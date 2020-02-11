fd8d48b75e9b817bb12befc0924150eba1ed0874
SilaevD <silaevd@yandex.ru>
2020-02-11 17:56:06 +0300

    add pagination

--- src/components/SectionsList.vue
+++ src/components/SectionsList.vue
@@ -34,7 +34,7 @@
                             <th>Created</th>
                             <th>Actions</th>
                         </tr>
-                        <tr v-for="(item, id) in filterBy(sections, sectionsSearch, 'name')"
+                        <tr v-for="(item, id) in filterBy(pagination, sectionsSearch, 'name')"
                             v-bind:key="id">
                             <td>{{ item.id }}</td>
                             <td>{{ item.name }}</td>
@@ -67,6 +67,12 @@
                         </tbody>
                     </table>
                 </div>
+                <div class="pagination">
+                    <div class="page"
+                         v-for="page in pages"
+                         :key="page"
+                         @click="pageClick(page)">{{ page }}</div>
+                </div>
 
             </div>
         </div>
@@ -84,9 +90,11 @@
         data() {
             return {
                 sectionsSearch: '',
-                // url: 'http://localhost:8080/data.json',
-                url: 'http://localhost/v1/sections',
+                url: 'http://localhost:8080/data.json',
+                // url: 'http://localhost/v1/sections',
                 sections: [],
+                sectionsPerPage: 10,
+                pageNumber: 1
 
             }
         },
@@ -94,7 +102,19 @@
 
         },
         computed:{
-
+            pages() {
+                return Math.ceil(this.sections.length / 10);
+            },
+            pagination() {
+                let from = (this.pageNumber -1) * this.sectionsPerPage;
+                let to = from + this.sectionsPerPage;
+                return this.sections.slice(from , to);
+            }
+        },
+        methods: {
+          pageClick(page) {
+              this.pageNumber = page;
+          }
         },
         mounted() {
             axios
@@ -105,5 +125,35 @@
 </script>
 
 <style scoped lang="scss">
-
+    .pagination {
+        display: flex;
+        justify-content: flex-end;
+        margin: 15px 0;
+        :first-child {
+            border-left: solid 1px #cecece;
+            border-top-left-radius: 4px;
+            border-bottom-left-radius: 4px;
+        }
+        :last-child {
+            border-top-right-radius: 4px;
+            border-bottom-right-radius: 4px;
+        }
+        .page {
+            text-align: center;
+            width: 40px;
+            padding: 5px 10px;
+            border-right: solid 1px #cecece;
+            border-top: solid 1px #cecece;
+            border-bottom: solid 1px #cecece;
+            transition: 300ms;
+            &:hover {
+                /*cursor: pointer;*/
+                background: #cecece;
+                cursor: pointer;
+                box-shadow: 0 10px 10px -10px rgba(0, 0, 0, 0.5);
+                transition-duration: 0.2s;
+                transition-property: box-shadow;
+            }
+        }
+    }
 </style>
